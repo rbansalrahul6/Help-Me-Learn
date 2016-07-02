6935508c7b70270355320e2399773076d1d042e2
rbansalrahul6 <rbansalrahul6@gmail.com>
2016-07-02 19:21:01 +0530

    login system created

---
+++ app/src/main/java/com/example/rbansal/helpmelearn/ui/login/CreateAccountActivity.java
@@ -0,0 +1,187 @@
+package com.example.rbansal.helpmelearn.ui.login;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.rbansal.helpmelearn.R;
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class CreateAccountActivity extends AppCompatActivity {
+    private final String LOG_TAG = CreateAccountActivity.class.getSimpleName();
+    public final String FIREBASE_URL = "https://help-me-learn.firebaseio.com/";
+    private ProgressDialog mAuthProgressDialog;
+    private Firebase mFirebaseRef;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText mEditTextUsernameCreate, mEditTextEmailCreate, mEditTextPasswordCreate;
+    private String mUserName, mUserEmail, mPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        Firebase.setAndroidContext(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mFirebaseRef = new Firebase(FIREBASE_URL);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(LOG_TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(LOG_TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
+
+        /**
+         * Link layout elements from XML and setup the progress dialog
+         */
+        initializeScreen();
+
+
+    }
+    public void initializeScreen() {
+        mEditTextUsernameCreate = (EditText) findViewById(R.id.edit_text_username_create);
+        mEditTextEmailCreate = (EditText) findViewById(R.id.edit_text_email_create);
+        mEditTextPasswordCreate = (EditText) findViewById(R.id.edit_text_password_create);
+        LinearLayout linearLayoutCreateAccountActivity = (LinearLayout) findViewById(R.id.linear_layout_create_account_activity);
+        //initializeBackground(linearLayoutCreateAccountActivity);
+
+        /* Setup the progress dialog that is displayed later when authenticating with Firebase */
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle(getResources().getString(R.string.progress_dialog_loading));
+        mAuthProgressDialog.setMessage(getResources().getString(R.string.progress_dialog_creating_user_with_firebase));
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    /**
+     * Open LoginActivity when user taps on "Sign in" textView
+     */
+    public void onSignInPressed(View view) {
+        Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * Create new account using Firebase email/password provider
+     */
+    public void onCreateAccountPressed(View view) {
+        mUserName = mEditTextUsernameCreate.getText().toString();
+        mUserEmail = mEditTextEmailCreate.getText().toString();
+        mPassword = mEditTextPasswordCreate.getText().toString();
+        //client side checking
+        boolean validEmail = isEmailValid(mUserEmail);
+        boolean validUserName = isUserNameValid(mUserName);
+        boolean validPassword = isPasswordValid(mPassword);
+        if (!validEmail || !validUserName || !validPassword) return;
+
+        /**
+         * If everything was valid show the progress dialog to indicate that
+         * account creation has started
+         */
+        mAuthProgressDialog.show();
+
+        /**
+         * Create new user with specified email and password
+         */
+        mAuth.createUserWithEmailAndPassword(mUserEmail, mPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            mAuthProgressDialog.dismiss();
+                            showToast("Account Created Successfully");
+                            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                            startActivity(intent);
+
+                        }
+                        else {
+                            Log.w(LOG_TAG, "signInWithEmail", task.getException());
+                            mAuthProgressDialog.dismiss();
+                            showToast("Authentification Failed");
+                        }
+                    }
+                });
+
+    }
+
+    /**
+     * Creates a new user in Firebase from the Java POJO
+     */
+    private void createUserInFirebaseHelper(final String encodedEmail) {
+    }
+
+    private boolean isEmailValid(String email) {
+        boolean isGoodEmail =
+                (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEditTextEmailCreate.setError(String.format(getString(R.string.error_invalid_email_not_valid),
+                    email));
+            return false;
+        }
+        return isGoodEmail;
+    }
+
+    private boolean isUserNameValid(String userName) {
+        if (userName.equals("")) {
+            mEditTextUsernameCreate.setError(getResources().getString(R.string.error_cannot_be_empty));
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isPasswordValid(String password) {
+        if (password.length() < 6) {
+            mEditTextPasswordCreate.setError(getResources().getString(R.string.error_invalid_password_not_valid));
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Show error toast to users
+     */
+    private void showToast(String message) {
+        Toast.makeText(CreateAccountActivity.this, message, Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/rbansal/helpmelearn/ui/login/LoginActivity.java
@@ -0,0 +1,184 @@
+package com.example.rbansal.helpmelearn.ui.login;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.rbansal.helpmelearn.R;
+import com.example.rbansal.helpmelearn.ui.MainActivity;
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String LOG_TAG = LoginActivity.class.getSimpleName();
+    /* A dialog that is presented until the Firebase authentication finished. */
+    private ProgressDialog mAuthProgressDialog;
+    private EditText mEditTextEmailInput, mEditTextPasswordInput;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Firebase.setAndroidContext(this);
+        mAuth = FirebaseAuth.getInstance();
+        // ...
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(LOG_TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(LOG_TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
+        /**
+         * Link layout elements from XML and setup progress dialog
+         */
+        initializeScreen();
+
+        /**
+         * Call signInPassword() when user taps "Done" keyboard action
+         */
+        mEditTextPasswordInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+
+                if (actionId == EditorInfo.IME_ACTION_DONE || keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                    signInPassword();
+                }
+                return true;
+            }
+        });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    /**
+     * Override onCreateOptionsMenu to inflate nothing
+     *
+     * @param menu The menu with which nothing will happen
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+
+    /**
+     * Sign in with Password provider when user clicks sign in button
+     */
+    public void onSignInPressed(View view) {
+        String email = mEditTextEmailInput.getText().toString();
+        String password = mEditTextPasswordInput.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.d(LOG_TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                            Toast.makeText(LoginActivity.this,"Login successfull",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Log.w(LOG_TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Open CreateAccountActivity when user taps on "Sign up" TextView
+     */
+    public void onSignUpPressed(View view) {
+        Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Link layout elements from XML and setup the progress dialog
+     */
+    public void initializeScreen() {
+        mEditTextEmailInput = (EditText) findViewById(R.id.edit_text_email);
+        mEditTextPasswordInput = (EditText) findViewById(R.id.edit_text_password);
+        LinearLayout linearLayoutLoginActivity = (LinearLayout) findViewById(R.id.linear_layout_login_activity);
+        //initializeBackground(linearLayoutLoginActivity);
+        /* Setup the progress dialog that is displayed later when authenticating with Firebase */
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle(getString(R.string.progress_dialog_loading));
+        mAuthProgressDialog.setMessage(getString(R.string.progress_dialog_authenticating_with_firebase));
+        mAuthProgressDialog.setCancelable(false);
+        /* Setup Google Sign In */
+    }
+
+
+    /**
+     * Sign in with Password provider (used when user taps "Done" action on keyboard)
+     */
+    public void signInPassword() {
+    }
+
+    /**
+     * Helper method that makes sure a user is created if the user
+     * logs in with Firebase's email/password provider.
+     * @param authData AuthData object returned from onAuthenticated
+     */
+
+  //  private void setAuthenticatedUserPasswordProvider(AuthData authData) {
+  //  }
+
+    /*
+     * Helper method that makes sure a user is created if the user
+     * logs in with Firebase's Google login provider.
+     * @param authData AuthData object returned from onAuthenticated
+     */
+
+}
