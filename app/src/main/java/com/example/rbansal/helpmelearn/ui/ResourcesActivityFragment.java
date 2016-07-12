0904d99c16b91c61bfd7155a2f16cbe690104bca
rbansalrahul6 <rbansalrahul6@gmail.com>
2016-07-12 17:32:14 +0530

    file uploading added

--- app/src/main/java/com/example/rbansal/helpmelearn/ui/ResourcesActivityFragment.java
+++ app/src/main/java/com/example/rbansal/helpmelearn/ui/ResourcesActivityFragment.java
@@ -1,23 +1,44 @@
 package com.example.rbansal.helpmelearn.ui;
 
+import android.app.Activity;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.rbansal.helpmelearn.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.nononsenseapps.filepicker.FilePickerActivity;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ResourcesActivityFragment extends Fragment {
+    private static final String LOG_TAG = ResourcesActivity.class.getSimpleName();
     private Context mContext;
+    public static final int FILE_CODE = 111;
+    private Uri fileUri = null;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
 
     public ResourcesActivityFragment() {
     }
@@ -27,6 +48,8 @@ public class ResourcesActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_resources, container, false);
         mContext = getActivity();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReferenceFromUrl("gs://help-me-learn.appspot.com");
         Button addBtn = (Button) rootView.findViewById(R.id.add_resource);
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,9 +70,81 @@ public class ResourcesActivityFragment extends Fragment {
                 // internal memory.
                 i.putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().getPath());
 
-                startActivityForResult(i, -1);
+                startActivityForResult(i,FILE_CODE);
             }
         });
         return rootView;
+    }
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == FILE_CODE && resultCode == Activity.RESULT_OK) {
+            if (data.getBooleanExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false)) {
+                // For JellyBean and above
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    ClipData clip = data.getClipData();
+
+                    if (clip != null) {
+                        for (int i = 0; i < clip.getItemCount(); i++) {
+                            Uri uri = clip.getItemAt(i).getUri();
+                            // Do something with the URI
+                        }
+                    }
+                    // For Ice Cream Sandwich
+                } else {
+                    ArrayList<String> paths = data.getStringArrayListExtra
+                            (FilePickerActivity.EXTRA_PATHS);
+
+                    if (paths != null) {
+                        for (String path: paths) {
+                            Uri uri = Uri.parse(path);
+                            // Do something with the URI
+                        }
+                    }
+                }
+
+            } else {
+                fileUri = data.getData();
+                // Do something with the URI
+            }
+            Log.v(LOG_TAG,fileUri.toString());
+            //uploading the file
+            StorageReference fileRef = storageRef.child("Test/" + fileUri.getLastPathSegment());
+            // Create the file metadata
+            StorageMetadata metadata = new StorageMetadata.Builder()
+                    .setContentType("txt")
+                    .build();
+
+// Upload file and metadata to the path 'images/mountains.jpg'
+            UploadTask uploadTask = fileRef.putFile(fileUri, metadata);
+
+// Listen for state changes, errors, and completion of the upload.
+            uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    System.out.println("Upload is " + progress + "% done");
+                }
+            }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onPaused(UploadTask.TaskSnapshot taskSnapshot) {
+                    System.out.println("Upload is paused");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Log.v(LOG_TAG,"Upload error");
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // Handle successful uploads on complete
+                    Uri downloadUrl = taskSnapshot.getMetadata().getDownloadUrl();
+                    Log.v("download",downloadUrl.toString());
+                }
+            });
+
+        }
     }
 }
